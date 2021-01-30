20e0a62be0fd0f72b33427ff4d91d333724f6193
Scott Alton <scottaltonmusic@gmail.com>
2021-01-30 13:38:05 -0500

    About page content added.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # WEBD6201-W2021-LAB1-Nick-Scott
 
 This is our groups lab 1 
+
+i) Home 
+ii) Projects
+iii) About 
+iv) Services
+v) Contact 
--- Scripts/app.js
+++ Scripts/app.js
@@ -19,8 +19,17 @@ let myContact =
 
 (function()
 {
+
     function displayHome()
     {
+      function bgFunction()
+      {
+      //  mainContent.insertAdjacentHTML(mainContent, "<p>This was added by a butto</p>")
+        document.body.style.backgroundImage = "url('https://images.pexels.com/photos/1658967/pexels-photo-1658967.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')";
+      }
+      document.body.style.backgroundColor = "#F3F3F3";
+      //document.querySelector("main").style.background = "url(../Assets/finnlogo.ico)";
+      
         let paragraphOneText =
           "This is a simple site to demonstrate DOM Manipulation for ICE 1";
 
@@ -66,14 +75,66 @@ let myContact =
         //mainContent.firstElementChild.textContent = "Welcome Home!";
 
         mainContent.innerHTML = `<h1 id="firstHeading">Welcome to WEBD6201 - Lab 1</h1>
+        <button type="button" id="updateBackground" class="btn-lg btn-dark">Update Background Colour!</button>
          <p id="paragraphOne" class="fs-3 fw-bold">This is my first Paragraph</p>
         `;
+
+      const backgroundButton = document.querySelector('#updateBackground');
+
+      backgroundButton.addEventListener("click", bgFunction);
         
     }
 
     function displayAbout()
     {
+      // Grab container element
+      const container = document.querySelector(".container");
+      // Heading
+      const heading = document.createElement("h1");
+      heading.innerText = "Meet Scott & Nick";
+      heading.style.textDecoration = "underline";
+      heading.style.textDecorationColor = "green";
+      container.appendChild(heading);
+      
+      // About Scott 
+      const aboutContainer = document.createElement("div");
+      const aboutTextContainer = document.createElement("div");
+      const aboutText = document.createElement("p");
+      aboutTextContainer.classList.add("col-lg-6", "p-4");
+      aboutText.innerText = "Scott is a full-stack developer based in Whitby, Ontario. I enjoy woring with JavaScript, Ruby on Rails,\
+       and C#. Before coming to Durham College and pursuing a change in career direction, I worked as a cook and musician, and also \
+       teach guitar and piano lessons. I love creating things from scratching and breaking down complex problems.";
+      
+      aboutTextContainer.appendChild(aboutText);
+      
+      
+      // Button to view Scott resume
+      const resumeButton = document.createElement("btn");
+      resumeButton.innerText = "View Resume";
+      resumeButton.classList.add("btn", "btn-success");
+      aboutTextContainer.appendChild(resumeButton);
 
+       const aboutImage = document.createElement("div");
+      aboutImage.innerHTML = `<img src="../Assets/scottaltonpic.jpg" class="col-lg-6" />`
+      aboutImage.classList.add("col-lg-6");
+      aboutContainer.classList.add("row");
+      
+      aboutContainer.innerHTML = "<h3>Scott Alton</h3>";
+      aboutContainer.appendChild(aboutTextContainer);
+      aboutContainer.appendChild(aboutImage);
+      container.appendChild(aboutContainer);
+      
+      
+      // Scott picture
+      // Nick picture
+      // About Nick
+      // Button to view Nick resume
+      // Button to view projects and contact
+        let newParagraph = document.createElement("p");
+        let subheadingText = "About Scott & Nick";
+
+        let paragraphOneElement = document.getElementById("paragraphOne");
+  
     }
 
     function displayProjects()
